3f5f379399f3025c4db4dcb09035d006c89e95fc
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2023-01-26 14:50:51 +0100

    attempt to fix front test

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,18 @@ fn main() {
     let path: Option<String> = std::env::args().nth(1);
     match path {
         Some(x) => process(x),
-        None => println!("missing path as argument")
+        None => println!("missing path as argument"),
     }
 }
 
-fn process(path_string: String)  {
+fn process(path_string: String) {
     match get_content(&path_string) {
         Ok(content) => print_stats(content),
-        Err(error) => println!("{error}")
+        Err(error) => println!("{error}"),
     }
 }
 
 fn get_content(path_string: &str) -> Result<String, pdf_extract::OutputError> {
-
     let path: &std::path::Path = std::path::Path::new(&path_string);
     pdf_extract::extract_text(path)
 }
@@ -25,32 +24,43 @@ fn print_stats(content: String) {
     }
 }
 
-fn trim_nonalphabetical_front_and_back(source: &str) -> &str {
-    source
+fn trim_nonalphabetic_front_and_back(source: &str) -> &str {
+    let mut start_index: usize = 0;
+    for (index, item) in source.chars().enumerate() {
+        if item.is_alphabetic() {
+            start_index = index;
+            break;
+        }
+    }
+    &source[start_index..]
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn trim_does_not_modify_alphabetical() {
-        assert_eq!(trim_nonalphabetical_front_and_back("word"), "word");
+        assert_eq!(trim_nonalphabetic_front_and_back("word"), "word");
     }
 
     #[test]
     fn trim_removes_front() {
-        assert_eq!(trim_nonalphabetical_front_and_back("hello"), "12hello");
-        assert_eq!(trim_nonalphabetical_front_and_back("foo"), "-foo");
+        assert_eq!(trim_nonalphabetic_front_and_back("hello"), "12hello");
+        assert_eq!(trim_nonalphabetic_front_and_back("foo"), "-foo");
     }
- 
+
     #[test]
     fn trim_removes_back() {
-        assert_eq!(trim_nonalphabetical_front_and_back("hello"), "hello34");
-        assert_eq!(trim_nonalphabetical_front_and_back("foo"), "foo+");
+        assert_eq!(trim_nonalphabetic_front_and_back("hello"), "hello34");
+        assert_eq!(trim_nonalphabetic_front_and_back("foo"), "foo+");
     }
 
     #[test]
     fn preserved_middle() {
-        assert_eq!(trim_nonalphabetical_front_and_back("hello-world"), "13hello-world)");
+        assert_eq!(
+            trim_nonalphabetic_front_and_back("hello-world"),
+            "13hello-world)"
+        );
     }
 }
